3ae990796f45aba854fa56d2f65badfa9c96c6d6
niks321 <134275611+niks321@users.noreply.github.com>
2023-05-23 16:13:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+import First from './Components/First';
+import Navbar from './Components/Navbar';
+import StateBasic from './Components/StateBasic';
+import StateEg1 from './Components/StateEg1';
+import { Table } from '@mui/material';
+import Table1 from './Components/Table1';
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* <First/> */}
+      <Table1/>
+      
+    </div>
+  );
+}
+
+export default App;
